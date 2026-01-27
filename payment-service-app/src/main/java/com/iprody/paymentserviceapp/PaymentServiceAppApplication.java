1be2b72d0707a8f177a69b3de5f6477c2a4ec6ee
Samvel Pahalyants <spahalyants@gmail.com>
2026-01-27 13:36:00 +0300

    Induce a compilation error in the build

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/PaymentServiceAppApplication.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/PaymentServiceAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentServiceAppApplication {
 
-    public static void main(String[] args) {
+    public main(String[] args) {
         SpringApplication.run(PaymentServiceAppApplication.class, args);
     }
 
