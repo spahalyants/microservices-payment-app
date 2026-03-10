f3a03a20f3752e2d1da46a72a8595a4d0167c52d
Samvel Pahalyants <spahalyants@gmail.com>
2026-03-10 18:34:57 +0300

    feat: add logging to PaymentController and configure Actuator

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/security/SecurityConfig.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 .sessionManagement(sm ->
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
